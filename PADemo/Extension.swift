2bbf5039bbeedae643d79346b4c3756ad9f8e6e1
liushuo199 <liushuo199@pingan.com.cn>
2018-02-28 09:53:44 +0800

    PADemo_gitLearnTest commit 09:53

--- PADemo/Extension.swift
+++ PADemo/Extension.swift
@@ -8,35 +8,9 @@
 
 import UIKit
 
-
-
 extension UIViewController: ViewControllerMaker {
-   
-//    static func makeViewController(name: String) -> UIViewController? {
-//        if let nameSpace = Bundle.main.infoDictionary?["CFBundleExecutable"] as? String {
-//            if let viewControllerClass = NSClassFromString(nameSpace + "." + name) as? UIViewController.Type {
-//                let viewController = viewControllerClass.init()
-//                return viewController
-//            }
-//        }
-//        return nil
-//    }
-//    
-//    static func viewControllerClass(with name: String) -> UIViewController.Type? {
-//        if let nameSpace = Bundle.main.infoDictionary?["CFBundleExecutable"] as? String {
-//            if let viewControllerClass = NSClassFromString(nameSpace + "." + name) as? UIViewController.Type {
-//                return viewControllerClass
-//            }
-//        }
-//        return nil
-//    }
-//
-//    var viewControllerClass: BaseViewController {
-//        return UIViewController()
-//    }
     
 }
-
 
 extension ViewControllerMaker {
     
