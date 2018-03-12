28cffbbfe722c529aeec2923b0bd09d2ff7c5ef1
liushuo199 <liushuo199@pingan.com.cn>
2018-03-12 17:14:59 +0800

    update

--- PADemo/WebView/WebViewController.swift
+++ PADemo/WebView/WebViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by shuo on 2017/5/16.
 //  Copyright © 2017年 shuo. All rights reserved.
-//
+//  WKWebView学习
 
 import UIKit
 import WebKit
@@ -12,6 +12,13 @@ import WebKit
 class WebViewController: BaseViewController {
     
     var webView: WKWebView!
+    var progressView: UIProgressView = {
+        let progressView = UIProgressView()
+        progressView.progressTintColor = UIColor.paOrange
+        progressView.trackTintColor = UIColor.green
+        progressView.frame = CGRect(x: 0, y: UIScreen.navigationHeight, width: UIScreen.width, height: 2)
+        return progressView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +26,32 @@ class WebViewController: BaseViewController {
         webView.frame = view.bounds
         view.backgroundColor = UIColor.green
         view.addSubview(webView)
+        view.addSubview(progressView)
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
         webView.navigationDelegate = self
         let request = URLRequest(url: urlEncode()!)
         webView.load(request)
         setupNavigationItem()
+        progressView.transform = CGAffineTransform.init(scaleX: 1.0, y: 1.5)
+    }
+    
+    deinit {
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+            if progressView.progress == 1 {
+                UIView.animate(withDuration: 0.25, delay: 0.3, animations: {
+                    self.progressView.transform = CGAffineTransform.init(scaleX: 1.0, y: 1.4)
+                }, completion: { _ in
+                    self.progressView.isHidden = true
+                })
+            }
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
     }
     
     func setupNavigationItem() {
@@ -64,7 +93,7 @@ class WebViewController: BaseViewController {
     
     // url编码
     func urlEncode() -> URL? {
-        let urlString = "https://www.baidu.com/s?wd=百度"
+        let urlString = "http://www.baidu.com/"
         if let encodeUrlStr = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
             debugLog("encodeUrlStr = \(encodeUrlStr)")
             if let decodeUrlString = encodeUrlStr.removingPercentEncoding {
@@ -81,15 +110,17 @@ extension WebViewController: WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Swift.Void) {
         print("decidePolicyFor navigationAction")
-        decisionHandler(.cancel)
+        decisionHandler(.allow)
     }
 
     func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Swift.Void) {
         print("decidePolicyFor navigationResponse")
-        decisionHandler(.cancel)
+        decisionHandler(.allow)
     }
     
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        progressView.transform = CGAffineTransform.init(scaleX: 1.0, y: 1.4)
+        progressView.isHidden = false
         webView.evaluateJavaScript("document.getElementsByTagName('body')[0].style.background='#FF6602'", completionHandler: nil)
         print("didStartProvisionalNavigation navigation")
     }
@@ -109,10 +140,12 @@ extension WebViewController: WKNavigationDelegate {
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         // webView.evaluateJavaScript("document.getElementsByTagName('body')[0].style.background='#FF6602'", completionHandler: nil)
+        progressView.isHidden = true
         print("didFinish navigation")
     }
 
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error){
+        progressView.isHidden = true
         print("didFail navigation")
     }
     
