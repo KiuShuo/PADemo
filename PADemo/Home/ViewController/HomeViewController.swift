a6a86704cc68cb4588dd0acf5384828158ffcae5
liushuo199 <liushuo199@pingan.com.cn>
2018-03-16 09:37:58 +0800

    update

--- PADemo/Home/ViewController/HomeViewController.swift
+++ PADemo/Home/ViewController/HomeViewController.swift
@@ -26,6 +26,12 @@ class HomeViewController: BaseViewController {
         return delegater
     }()
     fileprivate var baseNavgationController: PABaseNavigationController?
+    var viewModel: HomeViewModel = HomeViewModel() {
+        didSet {
+            updateUI()
+        }
+    }
+    
     
     func testMBProgressHUD(isShow: Bool) {
         if isShow {
@@ -54,23 +60,23 @@ class HomeViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "首页"
-            let footerView = UIView()
-            footerView.frame.size.height = 50
-            self.tableView.tableFooterView = footerView
-            self.tableView.tableFooterView?.backgroundColor = UIColor.green
-
-        //tableView.estimatedRowHeight = 44.0
-        //tableView.rowHeight = 44.0
+        let footerView = UIView()
+        footerView.frame.size.height = 50
+        self.tableView.tableFooterView = footerView
+        self.tableView.tableFooterView?.backgroundColor = UIColor.green
+        
         tableView.delegate = tableDelegater
         tableView.dataSource = tableDelegater
         
-        let _ = UIImage(named: "")
-        if let bundlePath = Bundle.main.path(forResource: "ControllerModel", ofType: "plist"),
-            let resultDic = NSDictionary(contentsOfFile: bundlePath) as? [String: Any],
-            let controllerModelDics = resultDic["result"] as? [[String: String]] {
-            self.controllerModels = Mapper<ControllerModel>().mapArray(JSONArray: controllerModelDics)
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "homeViewModelUpdate"), object: nil, queue: nil) { (notification) in
+            self.viewModel = HomeViewModel(controllerModels: self.viewModel.controllerModels)
         }
-        convertControllerModelsToSectionModels(controllerModels: controllerModels)
+        viewModel.fetchData()
+    }
+    
+    func updateUI() {
+        convertControllerModelsToSectionModels(controllerModels: viewModel.controllerModels)
+        tableView.reloadData()
     }
     
     func convertControllerModelsToSectionModels(controllerModels: [ControllerModel]) {
@@ -115,82 +121,3 @@ class HomeViewControllerTableDelegater: PATableDelegater {
     }
 
 }
-/*
-extension HomeViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return controllerModels.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "homeCell")
-        cell?.accessoryType = .detailButton
-        let controllerModel = controllerModels[indexPath.row]
-        cell?.textLabel?.text = controllerModel.identifier
-        return cell!
-    }
-    
-}
-
-extension HomeViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        let controllerModel = controllerModels[indexPath.row]
-        var toViewController: UIViewController?
-        
-        switch controllerModel.identifier {
-        case String(describing: CollectionViewController.self):
-            toViewController = CollectionViewController(collectionViewLayout: CollectionViewController.collectionViewLayout)
-        default:
-            if let controller = ControllerUtil.makeController(controllerModel: controllerModel) {
-                toViewController = controller
-            }
-        }
-    
-        guard let toVC = toViewController else {
-            return
-        }
-        
-//        baseNavgationController = PABaseNavigationController(rootViewController: toVC)
-//        let navgationItem = baseNavgationController?.customLeftBackButton(clickAction: { [weak self] in
-//            guard let`self` = self else { return }
-//            self.baseNavgationController?.dismiss(animated: true, completion: nil)
-//        })
-//        customScreenEdgePanGestureRecognizer(navigationController: baseNavgationController!)
-//        toVC.navigationItem.setLeftBarButton(navgationItem, animated: true)
-//        self.present(baseNavgationController!, animated: true, completion: nil)
-        
-        self.navigationController?.pushViewController(toVC, animated: true)
-    }
-    
-    func customScreenEdgePanGestureRecognizer(navigationController: UINavigationController) {
-        var a: UIRectEdge = .right
-         let coustomGes = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(screenEdgePan))
-        if let ges = getScreenEdgePanGestureRecognizer(view: navigationController.view) {
-            a = ges.edges
-            ges.require(toFail: coustomGes)
-        }
-       
-        coustomGes.edges = a
-        navigationController.view.addGestureRecognizer(coustomGes)
-    }
-    
-    func getScreenEdgePanGestureRecognizer(view: UIView) -> UIScreenEdgePanGestureRecognizer? {
-        if let gestureRecognizers = view.gestureRecognizers {
-            for ges in gestureRecognizers {
-                if ges is UIScreenEdgePanGestureRecognizer {
-                    return ges as? UIScreenEdgePanGestureRecognizer
-                }
-            }
-        }
-        return nil
-    }
-    
-    func screenEdgePan() {
-        baseNavgationController?.dismiss(animated: false, completion: nil)
-    }
-    
-}*/
-
---
+++ PADemo/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewModel.swift
+//  PADemo
+//
+//  Created by shuo on 2018/3/14.
+//  Copyright © 2018年 shuo. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class HomeViewModel {
+    
+    private(set) var controllerModels: [ControllerModel] {
+        didSet {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "homeViewModelUpdate"), object: nil)
+        }
+    }
+    
+//    var homdeDataDidChange = ObserverSet<Void>()
+    
+    init(controllerModels: [ControllerModel] = [ControllerModel]()) {
+        self.controllerModels = controllerModels
+    }
+    
+    // 获取数据
+    func fetchData() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            if let bundlePath = Bundle.main.path(forResource: "ControllerModel", ofType: "plist"),
+                let resultDic = NSDictionary(contentsOfFile: bundlePath) as? [String: Any],
+                let controllerModelDics = resultDic["result"] as? [[String: String]] {
+                self.controllerModels = Mapper<ControllerModel>().mapArray(JSONArray: controllerModelDics)
+            }
+        }
+    }
+    
+}
