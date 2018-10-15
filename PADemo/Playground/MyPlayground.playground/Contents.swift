ac77bbc4a34361af195d1ef94d564fe6566bfa4e
liushuo199 <liushuo199@pingan.com.cn>
2018-10-15 18:56:33 +0800

    update

--- PADemo/Playground/MyPlayground.playground/Contents.swift
+++ PADemo/Playground/MyPlayground.playground/Contents.swift
@@ -2,5 +2,50 @@
 
 import UIKit
 
+typealias Distance = Double
 
+struct Position {
+    var x: Double
+    var y: Double
+}
+
+extension Position {
+    
+    func whithin(range: Distance) -> Bool {
+        return sqrt(x * x + y * y) <= range
+    }
+    
+}
+
+struct Ship {
+    var position: Position
+    var firingRange: Distance
+    var unsafeRange: Distance
+}
+
+extension Ship {
+    
+    func canEngage(ship target: Ship) -> Bool {
+        let dx = target.position.x - position.x
+        let dy = target.position.y - position.y
+        let targetDistance = sqrt(dx * dx + dy * dy)
+        return targetDistance <= firingRange
+    }
+    
+    func canSafelyEngage(ship target: Ship, friendly: Ship) -> Bool {
+        let dx = target.position.x - position.x
+        let dy = target.position.y - position.y
+        let targetDistance = sqrt(dx * dx + dy * dy)
+        let friendlyDx = friendly.position.x - target.position.x
+        let friendlyDy = friendly.position.y - target.position.y
+        let friendlyDistance = sqrt(friendlyDx * friendlyDx + friendlyDy * friendlyDy)
+        
+        return targetDistance <= firingRange
+            && targetDistance > unsafeRange
+            && friendlyDistance > unsafeRange
+    }
+    
+    
+    
+}
 
